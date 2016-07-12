ab32ae5411249c56412118814ac7542193afd4b2
xuefu <252295126@qq.com>
2016-07-12 16:02:52 +0800

    添加Canvas和Paint

---
+++ AndroidCustomLayout/app/src/main/java/com/zxf/androidcustomlayout/CanvasAndPaint/CanvasAndPaintView1.java
@@ -0,0 +1,13 @@
+package com.zxf.androidcustomlayout.CanvasAndPaint;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/7/9.
+ */
+public class CanvasAndPaintView1 extends View {
+    public CanvasAndPaintView1(Context context) {
+        super(context);
+    }
+}
